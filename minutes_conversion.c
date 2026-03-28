7c490b59168caa1ce5769a65c93d982f4c656055
Parth Gawade <gawadeparth777@gmail.com>
2026-03-28 05:24:40 +0000

    initialization

---
+++ minutes_conversion.c
@@ -0,0 +1,10 @@
+//this program converts minutes to hours and minutes
+#include <stdio.h>
+int main() {
+	int minutes_entered;
+	double years=0.0;
+	double days=0.0;
+	double minutes_in_year=0.0;
+	double minutes_in_day=0.0;
+	printf("enter the no. of minutes:\n ");
+	scanf("%d",& minutes_entered);
