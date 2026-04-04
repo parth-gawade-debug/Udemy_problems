2381912cf646cc7a76ec3d9e7645ec998569bff5
Parth Gawade <gawadeparth777@gmail.com>
2026-04-04 09:47:09 +0000

    remove unnecessary comments

--- struct.c
+++ struct.c
@@ -9,21 +9,21 @@ struct Person {
 int main() {
     struct Person person1; // create an instance of the Person structure
     // assign values to the members of person1
-    person1.salary = 50000.0; // assign salary
-    person1.age = 30; // assign age
-    strcpy(person1.name, "John Doe");
+    person1.salary = 50000.0; 
+    person1.age = 30; 
+    strcpy(person1.name, "Parth");
     // print the values of the members of person1
-    printf("Name: %s\n", person1.name); // print name
-    printf("Age: %d\n", person1.age); // print age
-    printf("Salary: %.2f\n", person1.salary); // print salary
+    printf("Name: %s\n", person1.name); 
+    printf("Age: %d\n", person1.age); 
+    printf("Salary: %.2f\n", person1.salary); 
     struct Person person2; // create another instance of the Person structure
     // assign values to the members of person2
-    person2.salary = 60000.0; // assign salary
-    person2.age = 35; // assign age
-    strcpy(person2.name, "Jane Smith");
+    person2.salary = 60000.0; 
+    person2.age = 35; 
+    strcpy(person2.name, "Gawade");
     // print the values of the members of person2
-    printf("Name: %s\n", person2.name); // print name
-    printf("Age: %d\n", person2.age); // print age
-    printf("Salary: %.2f\n", person2.salary); // print salary
+    printf("Name: %s\n", person2.name); 
+    printf("Age: %d\n", person2.age); 
+    printf("Salary: %.2f\n", person2.salary); 
     return 0;
 }
