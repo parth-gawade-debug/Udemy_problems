77b5062dee2d4b15015222705bde51e32abe7af3
Parth Gawade <gawadeparth777@gmail.com>
2026-04-14 03:53:10 +0000

    added a comment

--- upper_lowercase.c
+++ upper_lowercase.c
@@ -4,7 +4,8 @@ int main() {
     char ch;
     printf("Enter a character: ");
     scanf("%c", &ch);
-    if (ch >= 'A' && ch <= 'Z') {
+    if (ch >= 'A' && ch <= 'Z') { // so here it is comparing the ASCII values 
+        //if instead of ch>='A' we would have written ch>=65 then also  it would have worked because the ASCII value of 'A' is 65 and the ASCII value of 'Z' is 90
         printf("The character '%c' is an uppercase letter.\n", ch);
     } else if (ch >= 'a' && ch <= 'z') {
         printf("The character '%c' is a lowercase letter.\n", ch);
